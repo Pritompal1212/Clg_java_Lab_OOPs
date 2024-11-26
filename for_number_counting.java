6840beaca06f31999c49ddf007f12ba5a9155f08
Pritompal1212 <mspritom111@gmail.com>
2024-11-26 07:42:08 +0530

    11th

--- for_number_counting.java
+++ for_number_counting.java
@@ -1,5 +1,5 @@
 public class for_number_counting {
     public static void main(String[] args) {
-        System.out.println("last update : week 3 \n\n and program c \n\n commite number = 8th done");
+        System.out.println("last update : week 4 \n\n and program b \n\n commite number = 10th done");
     }
 }
