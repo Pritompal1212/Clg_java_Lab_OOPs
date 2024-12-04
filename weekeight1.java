f13ac0cd282be589c09ae092b66612bb3126581a
Pritompal1212 <mspritom111@gmail.com>
2024-12-04 20:37:44 +0530

    23th

---
+++ weekeight1.java
@@ -0,0 +1,28 @@
+public class weekeight1 {
+    
+
+//     Aim: To write a JAVA program that import and use the defined your package in the previous Problem
+ 
+// (i)   Creating a package:
+// Steps:
+// 1.    First declare the name of the package using package keyword Example:    package mypack;
+// 2.    Type the following program under this package statement. In package : class ,data, methods all are public
+// package mypack; public class box
+// {
+// public int l=10,b=20; public void display()
+// {
+// System.out.println(l); System.out.println(b);
+// }
+// }
+// 3.    Create sub directory with a name same that of package name under the current working directory by as follows.        d:\>md mypack
+// 4.    Under this subdirectory store the above program with a file name “box.java”.
+// (ii)   importing a package:
+// Steps:
+// 1.    packages can be accessed by using the import statement General form: import pack1[.pack2].(classname/*); Example: import java.io.*;
+// 
+
+public static void main(String[] args) {
+    System.out.println("hiiiii");
+}
+
+}
