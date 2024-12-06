030809cec22e59c029cd5abe35d14d774806a982
Pritompal1212 <mspritom111@gmail.com>
2024-12-06 22:18:23 +0530

    25th

---
+++ weekeight3.java
@@ -0,0 +1,33 @@
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.effect.ImageInput;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+
+public class weekeight3 extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // TODO Auto-generated method stub
+        Image img = new Image("https://picsum.photos/536/354");
+        ImageInput imginput = new ImageInput();
+        Rectangle rect = new Rectangle();
+        imginput.setSource(img);
+        imginput.setX(20);
+        imginput.setY(100);
+        Group root = new Group();
+        rect.setEffect(imginput);
+        root.getChildren().add(rect);
+        Scene scene = new Scene(root, 530, 500, Color.BLACK);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("ImageInput Example");
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {  
+        launch(args);  
+    }   
+}
